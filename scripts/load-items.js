fd1fa6691815b55bceb72b5f7f35cbc452e0b961
Umair Bhatti <umairbhatti1010@gmail.com>
2024-10-14 22:44:45 +0200

    refactor

--- scripts/load-items.js
+++ scripts/load-items.js
@@ -1,15 +1,16 @@
-async function loadHTML(id, url) {
+const loadHTML = async (id, url) => {
   try {
     const response = await fetch(url);
-    if (!response.ok) throw new Error('Network response was not ok');
-    const data = await response.text();
-    document.getElementById(id).innerHTML = data;
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    document.getElementById(id).innerHTML = await response.text();
   } catch (error) {
     console.error('Failed to load HTML content:', error);
   }
-}
+};
 
-// Load each item 
+// Load each item
 loadHTML('item44', './items/item44.html');
 loadHTML('item45', './items/item45.html');
 loadHTML('item46', './items/item46.html');
