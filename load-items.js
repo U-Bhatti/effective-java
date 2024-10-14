0a973ca59bd951125063202b9749206bf625ff2a
Umair Bhatti <umairbhatti1010@gmail.com>
2024-10-14 22:06:09 +0200

    test changes

--- load-items.js
+++ load-items.js
@@ -10,8 +10,8 @@ async function loadHTML(id, url) {
 }
 
 // Load each item 
-loadHTML('item44', './item44.html');
-loadHTML('item45', './item45.html');
-loadHTML('item46', './item46.html');
-loadHTML('item47', './item47.html');
-loadHTML('item48', './item48.html');
+loadHTML('item44', 'item44.html');
+loadHTML('item45', 'item45.html');
+loadHTML('item46', 'item46.html');
+loadHTML('item47', 'item47.html');
+loadHTML('item48', 'item48.html');
