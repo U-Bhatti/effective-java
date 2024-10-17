905f6a0e71a982deb671ece181f2c37f2ed86e96
Umair Bhatti <umairbhatti1010@gmail.com>
2024-10-17 22:40:22 +0200

    add new chapters

--- scripts/load-items.js
+++ scripts/load-items.js
@@ -11,8 +11,13 @@ const loadHTML = async (id, url) => {
 };
 
 // Load each item
-loadHTML('item44', './items/item44.html');
-loadHTML('item45', './items/item45.html');
-loadHTML('item46', './items/item46.html');
-loadHTML('item47', './items/item47.html');
-loadHTML('item48', './items/item48.html');
+// loadHTML('item44', './items/item44.html');
+// loadHTML('item45', './items/item45.html');
+// loadHTML('item46', './items/item46.html');
+// loadHTML('item47', './items/item47.html');
+// loadHTML('item48', './items/item48.html');
+loadHTML('item49', './items/item49.html');
+loadHTML('item50', './items/item50.html');
+loadHTML('item51', './items/item51.html');
+loadHTML('item52', './items/item52.html');
+loadHTML('item53', './items/item53.html');
